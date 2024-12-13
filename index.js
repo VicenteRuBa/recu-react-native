a86fce7d2c383285ddec9f2647fc31aa3f3dd5a7
VicenteRuBa <ruizbarreavicente@gmail.com>
2024-12-13 09:06:41 -0300

    otro algo

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { AppRegistry } from "react-native";
-import App from "./app"; // Importa tu componente principal
-import { name as appName } from "./app.json"; // Usa el nombre definido en app.json
+import { registerRootComponent } from "expo";
+import Layout from "./app/_layout";
 
-AppRegistry.registerComponent(appName, () => App);
+registerRootComponent(Layout);
